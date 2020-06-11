0e86b52bb3cfa291d3270803e2f02e36f1b80597
kivimango <balazsviktor23@gmail.com>
2020-06-12 01:09:46 +0200

    Added default theme to window

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use orbtk::prelude::*;
 mod gui;
 mod core;
 
+static DEFAULT_THEME: &'static str = include_str!("../res/theme/default.css");
+
+fn get_theme() -> ThemeValue {
+    ThemeValue::create()
+        .extension_css(theme::DEFAULT_THEME_CSS)
+        .extension_css(DEFAULT_THEME)
+        .build()
+}
+
 fn main() {
     Application::new()
         .window(|context| {
@@ -12,6 +21,7 @@ fn main() {
                 .position((100.0, 100.0))
                 .size(1920.0 * 0.775, 1080.0 * 0.75)
                 .resizeable(true)
+                .theme(get_theme())
                 .child(
                     Stack::create()
                         .orientation("horizontal")
