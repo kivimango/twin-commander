4ba8429a8c5faa8501d7ea422ae1b270efc06fa6
kivimango <balazsviktor23@gmail.com>
2022-11-27 10:03:32 +0100

    Add parent as the first entry of the list.Added selection methods

--- src/ui/table/table_model.rs
+++ src/ui/table/table_model.rs
@@ -21,13 +21,36 @@ impl TableViewModel {
         }
     }
 
+    pub(crate) fn cd(&mut self, new_path: PathBuf) -> Result<(), ()> {
+        if new_path.is_dir() {
+            self.cwd = new_path;
+            self.refresh();
+            return Ok(());
+        } else {
+            return Err(());
+        }
+    }
+
     pub(crate) fn files(&self) -> &Vec<DirContent> {
         &self.files
     }
 
     pub(crate) fn list(&mut self) -> Result<(), Error> {
         match list_dir(&self.cwd) {
-            Ok(files) => {
+            Ok(mut files) => {
+                // if the dir is not the root, add an .. entry to the result into the first place,
+                // to able to go back up one level
+                if let Some(_parent) = self.cwd.parent() {
+                    let parent = DirContent {
+                        name: String::from(".."),
+                        ext: String::new(),
+                        size: String::from("<DIR>"),
+                        is_dir: true,
+                        date: String::from("Date"),
+                        attrs: String::new(),
+                    };
+                    files.insert(0, parent);
+                }
                 self.files = files;
                 return Ok(());
             }
@@ -35,8 +58,24 @@ impl TableViewModel {
         }
     }
 
+    pub(crate) fn get_file(&self, index: usize) -> Option<&DirContent> {
+        self.files.get(index)
+    }
+
+    pub(crate) fn get_file_mut(&mut self, index: usize) -> Option<&mut DirContent> {
+        self.files.get_mut(index)
+    }
+
     pub(crate) fn pwd(&self) -> &Path {
         self.cwd.as_path()
+    }
+
+    pub(crate) fn set_cwd(&mut self, new_cwd: PathBuf) {
+        self.cwd = new_cwd;
+    }
+
+    pub(crate) fn selected(&self) -> Option<usize> {
+        self.state.selected()
     }
 
     pub(crate) fn select_previous(&mut self) {
@@ -69,5 +108,15 @@ impl TableViewModel {
 
     pub(crate) fn state_mut(&mut self) -> &mut TableState {
         &mut self.state
+    }
+
+    pub(crate) fn refresh(&mut self) {
+        if let Ok(files) = list_dir(&self.cwd) {
+            self.files = files;
+        }
+    }
+
+    pub(crate) fn reset_selection(&mut self) {
+        self.state.select(None);
     }
 }
