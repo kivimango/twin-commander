afff71d57f9d3438d0fe453686c3484918c7bf83
kivimango <balazsviktor23@gmail.com>
2022-12-06 12:00:58 +0100

    Application logic code moved to app.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 use ::termion::raw::IntoRawMode;
-use event::{Event, Events};
+use app::Application;
 use std::{error::Error, io::stdout};
-use termion::event::Key;
-use tui::{
-    backend::TermionBackend,
-    layout::{Constraint, Layout},
-    Terminal,
-};
-use ui::{Menu, TableView};
+use tui::{backend::TermionBackend, Terminal};
 
 mod app;
 mod core;
 mod event;
 mod ui;
-
-enum ActivePanel {
-    Left,
-    Right,
-}
-
-impl ActivePanel {
-    fn switch(&mut self) {
-        match self {
-            ActivePanel::Left => *self = ActivePanel::Right,
-            ActivePanel::Right => *self = ActivePanel::Left,
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Initializing terminal with termion backend
@@ -36,89 +16,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     terminal.clear()?;
     terminal.hide_cursor()?;
 
-    let events = Events::new(None);
-    let mut should_quit = false;
-
-    let mut menu = Menu::new();
-    let mut left_panel = TableView::new();
-    let mut right_panel = TableView::new();
-    left_panel.activate();
-    let mut active_panel = ActivePanel::Left;
-
-    loop {
-        if should_quit {
-            break;
-        }
-
-        terminal.draw(|frame| {
-            let frame_size = frame.size();
-
-            let layout = Layout::default()
-                .constraints([
-                    Constraint::Min(1),
-                    Constraint::Percentage(90),
-                    Constraint::Min(1),
-                ])
-                .direction(tui::layout::Direction::Vertical)
-                .split(frame_size);
-
-            menu.render(layout[0], frame);
-            left_panel.render_table(layout[1], 0, frame);
-            right_panel.render_table(layout[1], 1, frame);
-        })?;
-
-        if let Ok(event) = events.recv() {
-            match event {
-                Event::Input(key) => match key {
-                    Key::Esc => should_quit = true,
-                    Key::Char('\t') => {
-                        if left_panel.is_active() {
-                            left_panel.deactivate();
-                            right_panel.activate();
-                        } else {
-                            left_panel.activate();
-                            right_panel.deactivate();
-                        }
-                        active_panel.switch()
-                    }
-                    // Menu
-                    Key::F(9) => menu.select_next(),
-                    Key::Left => {
-                        if menu.has_selection() {
-                            menu.select_previous()
-                        }
-                    }
-                    Key::Right => {
-                        if menu.has_selection() {
-                            menu.select_next()
-                        }
-                    }
-                    Key::Home => match active_panel {
-                        ActivePanel::Left => left_panel.select_first(),
-                        ActivePanel::Right => right_panel.select_first(),
-                    },
-                    Key::End => match active_panel {
-                        ActivePanel::Left => left_panel.select_last(),
-                        ActivePanel::Right => right_panel.select_last(),
-                    },
-                    Key::Up => match active_panel {
-                        ActivePanel::Left => left_panel.select_previous(),
-                        ActivePanel::Right => right_panel.select_previous(),
-                    },
-                    Key::Down => match active_panel {
-                        ActivePanel::Left => left_panel.select_next(),
-                        ActivePanel::Right => right_panel.select_next(),
-                    },
-                    Key::Char('\n') => match active_panel {
-                        ActivePanel::Left => left_panel.change_dir(),
-                        ActivePanel::Right => right_panel.change_dir(),
-                    },
-                    _ => {}
-                },
-                Event::Tick => {}
-            }
-        }
-    }
+    let mut app = Application::new();
+    app.run(&mut terminal);
 
     // Restore terminal and close the application
     terminal.clear()?;
