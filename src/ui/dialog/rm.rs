20c21ead37c68be8b545f7cdb72a4991095986c8
kivimango <balazsviktor23@gmail.com>
2023-01-20 09:03:37 +0100

    Added key handling and state switching

--- src/ui/dialog/rm.rs
+++ src/ui/dialog/rm.rs
@@ -1,10 +1,12 @@
-use std::path::PathBuf;
-use termion::event::Key;
+use std::{io::Stdout, path::PathBuf};
+use termion::{event::Key, raw::RawTerminal};
 use tui::{
-    layout::Alignment,
-    style::{Color, Style, Modifier},
+    backend::TermionBackend,
+    layout::{Alignment, Rect},
+    style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
     widgets::{Block, Borders, Paragraph, Wrap},
+    Frame,
 };
 
 enum FocusedButton {
@@ -27,22 +29,36 @@ pub enum DeleteDialogState {
     Deleted,
 }
 
+impl Default for DeleteDialogState {
+    fn default() -> Self {
+        DeleteDialogState::WaitingForConfirmation
+    }
+}
+
 pub struct RmDirDialog {
-    path: Vec<PathBuf>,
+    dialog_state: DeleteDialogState,
     focused_button: FocusedButton,
+    path: Vec<PathBuf>,
 }
 
 impl RmDirDialog {
     pub fn new(path: Vec<PathBuf>) -> Self {
         RmDirDialog {
-            path,
+            dialog_state: DeleteDialogState::default(),
             focused_button: FocusedButton::Cancel,
+            path,
         }
     }
 
     pub fn handle_keys(&mut self, key: Key) {
         match key {
-            Key::Char('\n') => {}
+            Key::Char('\n') => match self.dialog_state {
+                DeleteDialogState::WaitingForConfirmation => match self.focused_button {
+                    FocusedButton::Ok => self.dialog_state = DeleteDialogState::Deleting,
+                    FocusedButton::Cancel => {}
+                },
+                _ => {}
+            },
             Key::Char('\t') | Key::Right | Key::Left | Key::Up | Key::Down => {
                 self.focused_button.next()
             }
@@ -50,7 +66,19 @@ impl RmDirDialog {
         }
     }
 
-    pub fn render(&self) -> Paragraph {
+    pub fn render(&self, frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>, area: Rect) {
+        match self.dialog_state {
+            DeleteDialogState::WaitingForConfirmation => self.show_confirmation_dialog(frame, area),
+            DeleteDialogState::Deleting => self.show_deleting_files(frame, area),
+            DeleteDialogState::Deleted => {}
+        }
+    }
+
+    fn show_confirmation_dialog(
+        &self,
+        frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>,
+        area: Rect,
+    ) {
         let button_titles = {
             match self.focused_button {
                 FocusedButton::Ok => ("[X] OK ", "[ ] Cancel"),
@@ -58,19 +86,23 @@ impl RmDirDialog {
             }
         };
         let spans = vec![
-            Spans::from(vec![
-                Span::styled(self.confirm_msg(), Style::default().fg(Color::White)),
-            ]),
-            Spans::from(vec![
-                Span::styled(self.get_name(), Style::default().fg(Color::Black).add_modifier(Modifier::BOLD))
-            ]),
+            Spans::from(vec![Span::styled(
+                self.confirm_msg(),
+                Style::default().fg(Color::White),
+            )]),
+            Spans::from(vec![Span::styled(
+                self.get_name(),
+                Style::default()
+                    .fg(Color::Black)
+                    .add_modifier(Modifier::BOLD),
+            )]),
             Spans::from(vec![
                 Span::styled(button_titles.0, Style::default().fg(Color::Black)),
                 Span::styled(button_titles.1, Style::default().fg(Color::Black)),
             ]),
         ];
         let text = Text::from(spans);
-        Paragraph::new(text)
+        let p = Paragraph::new(text)
             .block(
                 Block::default()
                     .title("Delete")
@@ -79,7 +111,32 @@ impl RmDirDialog {
             )
             .wrap(Wrap { trim: false })
             .style(Style::default().bg(Color::LightRed).fg(Color::Gray))
+            .alignment(Alignment::Center);
+        frame.render_widget(p, area);
+    }
+
+    fn show_deleting_files(
+        &self,
+        frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>,
+        area: Rect,
+    ) {
+        let spans = vec![
+            Spans::from(vec![Span::styled(
+                "Deleting:",
+                Style::default().fg(Color::Black),
+            )]),
+            Spans::from(vec![Span::styled(
+                self.get_name(),
+                Style::default().fg(Color::Black),
+            )]),
+        ];
+        let text = Text::from(spans);
+        let paragraph = Paragraph::new(text)
+            .block(Block::default().borders(Borders::ALL))
+            .style(Style::default().bg(Color::White).fg(Color::Black))
             .alignment(Alignment::Center)
+            .wrap(Wrap { trim: true });
+        frame.render_widget(paragraph, area);
     }
 
     /// Decides the confirmation message to be displayed to the user based on the type
