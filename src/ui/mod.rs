1de8dc114f70670b00e20cb0b49d947e45c5e665
kivimango <balazsviktor23@gmail.com>
2023-01-24 19:38:57 +0100

    Added fixed_height_centered_rect() to module

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,5 @@
+use tui::layout::{Constraint, Direction, Layout, Rect};
+
 mod bottom_menu;
 mod dialog;
 mod menu;
@@ -7,3 +9,53 @@ pub use self::bottom_menu::*;
 pub use self::dialog::*;
 pub use self::menu::*;
 pub use self::table::*;
+
+/// Helper function to create a centered rect with a fixed height
+/// and using up certain percentage of the available of width of `r`.
+pub fn fixed_height_centered_rect(percent_x: u16, height: u16, r: Rect) -> Rect {
+    let percent_y = 50;
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_y) / 2),
+                Constraint::Length(height),
+                Constraint::Percentage((100 - percent_y) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage((100 - percent_x) / 2),
+                Constraint::Percentage(percent_x),
+                Constraint::Percentage((100 - percent_x) / 2),
+            ]
+            .as_ref(),
+        )
+        .split(popup_layout[1])[1]
+}
+
+#[cfg(test)]
+mod test {
+    use super::fixed_height_centered_rect;
+    use tui::layout::Rect;
+
+    #[test]
+    fn test_fixed_height_centered_rect() {
+        let percent_x = 50;
+        let height = 10;
+        let available_area = Rect::new(0, 0, 100, 100);
+        let area = fixed_height_centered_rect(percent_x, height, available_area);
+
+        println!("{:?}", area);
+
+        assert_eq!(area.x, 25);
+        assert_eq!(area.y, 25);
+        assert_eq!(area.width, 50);
+        assert_eq!(area.height, height);
+    }
+}
