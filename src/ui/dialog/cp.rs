a218cde88509f533e7218f2e8928d669977522d7
kivimango <balazsviktor23@gmail.com>
2023-01-25 17:11:32 +0100

    Implemented new layout for CopyDialog

--- src/ui/dialog/cp.rs
+++ src/ui/dialog/cp.rs
@@ -2,10 +2,10 @@ use std::{io::Stdout, path::PathBuf};
 use termion::{event::Key, raw::RawTerminal};
 use tui::{
     backend::TermionBackend,
-    layout::{Alignment, Rect},
+    layout::{Alignment, Constraint, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
-    widgets::{Block, Borders, Paragraph, Wrap},
+    widgets::{Block, Borders, Paragraph},
     Frame,
 };
 
@@ -62,43 +62,54 @@ impl CopyDialog {
                 Buttons::Cancel => ("[ ] OK ", "[X] Cancel"),
             }
         };
-        let spans = vec![
-            Spans::from(vec![Span::styled(
-                "Source:",
-                Style::default().fg(Color::White),
-            )]),
-            Spans::from(vec![Span::styled(
-                self.source.display().to_string(),
-                Style::default()
-                    .fg(Color::Black)
-                    .add_modifier(Modifier::BOLD),
-            )]),
-            Spans::from(vec![Span::styled(
-                "Destination:",
-                Style::default().fg(Color::White),
-            )]),
-            Spans::from(vec![Span::styled(
-                self.destination.display().to_string(),
-                Style::default()
-                    .fg(Color::Black)
-                    .add_modifier(Modifier::BOLD),
-            )]),
-            Spans::from(vec![
-                Span::styled(button_titles.0, Style::default().fg(Color::Black)),
-                Span::styled(button_titles.1, Style::default().fg(Color::Black)),
-            ]),
-        ];
-        let text = Text::from(spans);
-        let p = Paragraph::new(text)
-            .block(
-                Block::default()
-                    .title("Copy file(s)")
-                    .borders(Borders::ALL)
-                    .style(Style::default().bg(Color::White).fg(Color::Black)),
-            )
-            .wrap(Wrap { trim: false })
-            .style(Style::default().bg(Color::White).fg(Color::Black))
-            .alignment(Alignment::Left);
-        frame.render_widget(p, area);
+        let dialog_area = Rect::new(area.x, area.y, area.width, area.height);
+        let layout = Layout::default()
+            .constraints([
+                Constraint::Length(1),
+                Constraint::Length(1),
+                Constraint::Length(1),
+                Constraint::Length(1),
+                Constraint::Length(1),
+            ])
+            .direction(tui::layout::Direction::Vertical)
+            .margin(1)
+            .split(area);
+
+        let block = Block::default()
+            .border_type(tui::widgets::BorderType::Rounded)
+            .borders(Borders::ALL)
+            .title("Copy file(s)")
+            .title_alignment(Alignment::Center);
+        let label_src = Paragraph::new(Text::styled("Source:", Style::default().fg(Color::White)));
+        let label_src_path = Paragraph::new(Text::styled(
+            self.source.display().to_string(),
+            Style::default()
+                .bg(Color::Cyan)
+                .fg(Color::White)
+                .add_modifier(Modifier::BOLD),
+        ));
+        let label_dest = Paragraph::new(Text::styled(
+            "Destination:",
+            Style::default().fg(Color::White),
+        ));
+        let label_dest_path = Paragraph::new(Text::styled(
+            self.destination.display().to_string(),
+            Style::default()
+                .bg(Color::Cyan)
+                .fg(Color::White)
+                .add_modifier(Modifier::BOLD),
+        ));
+        let buttons = Paragraph::new(Spans::from(vec![
+            Span::styled(button_titles.0, Style::default().fg(Color::White)),
+            Span::styled(button_titles.1, Style::default().fg(Color::White)),
+        ]))
+        .alignment(Alignment::Center);
+
+        frame.render_widget(block, dialog_area);
+        frame.render_widget(label_src, layout[0]);
+        frame.render_widget(label_src_path, layout[1]);
+        frame.render_widget(label_dest, layout[2]);
+        frame.render_widget(label_dest_path, layout[3]);
+        frame.render_widget(buttons, layout[4]);
     }
 }
