637b4937273ff4555035d61abaf404b1c1a69b9a
kivimango <balazsviktor23@gmail.com>
2023-04-04 18:28:52 +0200

    Added Widgets enum

--- src/ui/widgets/mod.rs
+++ src/ui/widgets/mod.rs
@@ -1,3 +1,11 @@
-mod widgets;
+mod panel;
 
-pub use self::widgets::*;
+pub use self::panel::*;
+
+/// A list of available widgets to use in a `Panel`.
+pub enum Widgets {
+    /// Displays the content of the working directory in a table-like format.
+    Table,
+    /// Displays the contents of a text file.
+    TextFileViewer
+}
