f7ce294d85be22d853e13ffa5b43fd78abc346cb
kivimango <balazsviktor23@gmail.com>
2023-04-04 19:28:00 +0200

    Added RenderWidget trait

--- src/ui/widgets/mod.rs
+++ src/ui/widgets/mod.rs
@@ -1,11 +1,26 @@
+use std::io::Stdout;
+use termion::raw::RawTerminal;
+use tui::{backend::TermionBackend, layout::Rect, Frame};
+
 mod panel;
+mod text_file_viewer;
 
 pub use self::panel::*;
+pub use self::text_file_viewer::*;
 
 /// A list of available widgets to use in a `Panel`.
 pub enum Widgets {
     /// Displays the content of the working directory in a table-like format.
     Table,
     /// Displays the contents of a text file.
-    TextFileViewer
+    TextFileViewer,
+}
+
+pub trait RenderWidget {
+    /// Renders the representation of the actual state into the terminal.
+    ///
+    /// # Parameters
+    /// * `area`: the available area to the widget for rendering its state
+    /// * `frame`: the actual frame of the rendering loop
+    fn render(&self, area: Rect, frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>);
 }
