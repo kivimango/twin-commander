d35c04cb128c359bce3a85fb8ad143e3cc7a7fd5
kivimango <balazsviktor23@gmail.com>
2023-10-30 10:19:49 +0100

    Added FilterOptions, impl From for DirContent, list_dir refactored iterator style

--- src/core/list_dir.rs
+++ src/core/list_dir.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Local};
-use std::fs;
+use std::fs::{self, DirEntry};
 use std::io::Error;
 use std::path::Path;
 
@@ -13,65 +13,100 @@ pub struct DirContent {
     pub attrs: String,
 }
 
+impl From<DirEntry> for DirContent {
+    fn from(entry: DirEntry) -> Self {
+        let name = match entry.file_name().into_string() {
+            Ok(fname) => fname,
+            Err(error) => {
+                eprintln!("NOTICE: cannot read filename: {:?}", error);
+                "N/A".to_string()
+            }
+        };
+
+        let mut is_dir = true;
+        let mut size = None;
+        let (is_dir, size, date, attrs) = match entry.metadata() {
+            Ok(metadata) => {
+                if !metadata.is_dir() {
+                    is_dir = false;
+                    size = Some(metadata.len());
+                }
+
+                let date = match metadata.modified() {
+                    Ok(modified) => {
+                        let datetime_local: DateTime<Local> = modified.into();
+                        let dt_formatted = datetime_local.format("%Y.%m.%d %H:%M");
+                        dt_formatted.to_string()
+                    }
+                    Err(e) => {
+                        eprintln!(
+                            "NOTICE: cannot read last modification date for {}, error: {}",
+                            &name, e
+                        );
+                        "N/A".to_string()
+                    }
+                };
+
+                let attrs = "".to_string();
+
+                (is_dir, size, date, attrs)
+            }
+            Err(error) => {
+                eprintln!("NOTICE: cannot read file metadata: {:?}", error);
+                (is_dir, None, String::new(), String::new())
+            }
+        };
+
+        DirContent {
+            name,
+            is_dir,
+            size,
+            date,
+            attrs,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct FilterOptions {
+    pub show_hidden_files: bool,
+}
+
+impl FilterOptions {
+    pub fn new() -> Self {
+        FilterOptions::default()
+    }
+
+    pub fn show_hidden_files(&self) -> bool {
+        self.show_hidden_files
+    }
+
+    pub fn set_show_hidden_files(&mut self, show: bool) {
+        self.show_hidden_files = show;
+    }
+}
+
 /// Collects list of files and directories with their metadata in the given `path`
 /// into a vector of `DirContent`.
 /// I/O errors related to the `path` are propagated up to the caller.
 /// I/O errors related to filename and last modification date will not interrupt listing,
 /// instead those values will be replaced with a placeholder indicating that the function failed to
 /// read those values.
-pub fn list_dir(dir: &Path) -> Result<Vec<DirContent>, Error> {
-    let mut result: Vec<DirContent> = Vec::new();
-
-    for entry in fs::read_dir(dir)? {
-        let dir = entry?;
-        let metadata = dir.metadata()?;
-
-        let (name, is_dir, size, date, attrs) = {
-            let f_name = match dir.file_name().into_string() {
-                Ok(name) => name,
-                Err(e) => {
-                    eprintln!("NOTICE: cannot read filename, error: {:?}", e);
-                    "N/A".to_string()
-                }
-            };
-
-            let mut is_dir = true;
-            let mut size = None;
-
-            if !metadata.is_dir() {
-                is_dir = false;
-                size = Some(metadata.len());
+pub fn list_dir(dir: &Path, filter_options: &FilterOptions) -> Result<Vec<DirContent>, Error> {
+    let result: Vec<DirContent> = fs::read_dir(dir)?
+        .into_iter()
+        .filter(|result| result.is_ok())
+        // is it safe to unwarp here, we already have only the ok variants
+        .map(|entry| entry.unwrap())
+        .filter(|entry| {
+            if filter_options.show_hidden_files {
+                true
+            } else {
+                !entry.file_name().to_string_lossy().starts_with(".")
             }
+        })
+        .map(|entry| DirContent::from(entry))
+        .collect();
 
-            let date = match metadata.modified() {
-                Ok(modified) => {
-                    let datetime_local: DateTime<Local> = modified.into();
-                    let dt_formatted = datetime_local.format("%Y.%m.%d %H:%M");
-                    dt_formatted.to_string()
-                }
-                Err(e) => {
-                    eprintln!(
-                        "NOTICE: cannot read last modification date for {}, error: {}",
-                        &f_name, e
-                    );
-                    "N/A".to_string()
-                }
-            };
-
-            let attrs = "".to_string();
-
-            (f_name, is_dir, size, date, attrs)
-        };
-
-        let file_details = DirContent {
-            name,
-            is_dir,
-            size,
-            date,
-            attrs,
-        };
-        result.push(file_details);
-    }
-
-    return Ok(result);
+    Ok(result)
 }
