b82355a65f333e313d46845b0c6baf773d0628d9
kivimango <balazsviktor23@gmail.com>
2024-02-21 10:28:09 +0100

    Init terminal with the tuirealm terminal bridge

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use ::termion::raw::IntoRawMode;
 use app::Application;
-use std::{error::Error, io::stdout};
-use tui::{backend::TermionBackend, Terminal};
+use std::error::Error;
+use tuirealm::terminal::TerminalBridge;
 
 mod app;
 mod core;
@@ -9,18 +8,17 @@ mod event;
 mod ui;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Initializing terminal with termion backend
-    let stdout = stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
-    terminal.hide_cursor()?;
+    // Initializing terminal with termion terminal backend and ratatui renderer
+    let mut terminal = TerminalBridge::new()?;
+    terminal.clear_screen()?;
+    terminal.raw_mut().hide_cursor()?;
 
+    // Initialize the app and run the event loop
     let mut app = Application::new();
     app.run(&mut terminal);
 
     // Restore terminal and close the application
-    terminal.clear()?;
-    terminal.show_cursor()?;
+    terminal.raw_mut().clear()?;
+    terminal.raw_mut().show_cursor()?;
     Ok(())
 }
