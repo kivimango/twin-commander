9c4a3210b372e30dc63eebea2e9b771b7b0d08fc
kivimango <balazsviktor23@gmail.com>
2024-03-17 08:52:20 +0100

    Refactor BottomMenu to be a tuirealm component

--- src/ui/bottom_menu.rs
+++ src/ui/bottom_menu.rs
@@ -1,45 +1,129 @@
-use std::io::Stdout;
-use termion::raw::RawTerminal;
-use tui::{backend::TermionBackend, layout::Rect, Frame};
+use super::TopMenuMessage;
+use crate::app::ApplicationMessage;
+use tuirealm::{
+    command::{Cmd, CmdResult},
+    event::{Key, KeyEvent, KeyModifiers},
+    props::{Color, Style},
+    tui::{
+        layout::Rect,
+        text::{Line, Span},
+        widgets::Tabs,
+    },
+    AttrValue, Attribute, Component, Event, Frame, MockComponent, NoUserEvent, Props, State,
+};
 
-pub struct BottomMenu {}
+pub struct BottomMenu {
+    properties: Props,
+    labels: [&'static str; 10],
+}
 
 impl BottomMenu {
     pub fn new() -> Self {
-        BottomMenu {}
+        BottomMenu {
+            properties: Props::default(),
+            labels: [
+                " 1Help", " 2Menu", " 3View", " 4Edit", " 5Copy", " 6Move", " 7New", " 8Del",
+                " 9Menu", "10Quit",
+            ],
+        }
     }
 
-    pub fn render(
-        &self,
-        main_layout: Rect,
-        frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>,
-    ) {
-        let menu_bottom_items = [
-            " 1Help", " 2Menu", " 3View", " 4Edit", " 5Copy", " 6Move", " 7New", " 8Del", " 9Menu",
-            "10Quit",
-        ]
-        .iter()
-        .map(|item| {
-            let (first, rest) = item.split_at(2);
-            tui::text::Spans::from(vec![
-                tui::text::Span::styled(
-                    first,
-                    tui::style::Style::default()
-                        .fg(tui::style::Color::White)
-                        .bg(tui::style::Color::Black),
-                ),
-                tui::text::Span::styled(
-                    rest,
-                    tui::style::Style::default()
-                        .fg(tui::style::Color::Black)
-                        .bg(tui::style::Color::Cyan),
-                ),
-            ])
-        })
-        .collect();
-        let menu_bottom = tui::widgets::Tabs::new(menu_bottom_items)
-            .style(tui::style::Style::default().bg(tui::style::Color::Cyan))
-            .divider(tui::text::Span::raw(" "));
-        frame.render_widget(menu_bottom, main_layout);
+    /// Sets the background color of the buttons in the menu
+    pub fn background(mut self, bacground: Color) -> Self {
+        self.properties
+            .set(Attribute::Background, AttrValue::Color(bacground));
+        self
+    }
+
+    /// Sets the text color of the menu items in the menu
+    pub fn label_foreground(mut self, foreground: Color) -> Self {
+        self.properties
+            .set(Attribute::Foreground, AttrValue::Color(foreground));
+        self
+    }
+
+    /// Sets the function key highlight background color of the menu items in the menu
+    pub fn function_key_background(mut self, background: Color) -> Self {
+        self.properties
+            .set(Attribute::HighlightedColor, AttrValue::Color(background));
+        self
+    }
+
+    /// Sets the function key highlight text color of the menu items in the menu
+    pub fn function_key_foreground(mut self, foreground: Color) -> Self {
+        self.properties
+            .set(Attribute::Color, AttrValue::Color(foreground));
+        self
+    }
+}
+
+impl MockComponent for BottomMenu {
+    fn attr(&mut self, attr: Attribute, value: AttrValue) {
+        self.properties.set(attr, value)
+    }
+
+    fn query(&self, query: Attribute) -> Option<AttrValue> {
+        self.properties.get(query)
+    }
+
+    fn state(&self) -> State {
+        State::None
+    }
+
+    fn perform(&mut self, _cmd: Cmd) -> CmdResult {
+        CmdResult::None
+    }
+
+    fn view(&mut self, frame: &mut Frame, area: Rect) {
+        let bacground = self
+            .properties
+            .get_or(Attribute::Background, AttrValue::Color(Color::Cyan))
+            .unwrap_color();
+        let foreground = self
+            .properties
+            .get_or(Attribute::Foreground, AttrValue::Color(Color::Black))
+            .unwrap_color();
+        let highlight_bg = self
+            .properties
+            .get_or(Attribute::HighlightedColor, AttrValue::Color(Color::Black))
+            .unwrap_color();
+        let higlight_fg = self
+            .properties
+            .get_or(Attribute::Color, AttrValue::Color(Color::White))
+            .unwrap_color();
+
+        let menu_bottom_items = self
+            .labels
+            .iter()
+            .map(|item| {
+                let (first, rest) = item.split_at(2);
+                Line::from(vec![
+                    Span::styled(first, Style::default().fg(higlight_fg).bg(highlight_bg)),
+                    Span::styled(rest, Style::default().fg(foreground)),
+                ])
+            })
+            .collect();
+
+        let bottom_menu = Tabs::new(menu_bottom_items)
+            .style(Style::default().bg(bacground))
+            .divider(Span::raw(" "));
+
+        frame.render_widget(bottom_menu, area);
+    }
+}
+
+impl Component<ApplicationMessage, NoUserEvent> for BottomMenu {
+    fn on(&mut self, event: Event<NoUserEvent>) -> Option<ApplicationMessage> {
+        match event {
+            Event::Keyboard(KeyEvent {
+                code: Key::Function(9),
+                modifiers: KeyModifiers::NONE,
+            }) => Some(ApplicationMessage::TopMenu(TopMenuMessage::Focus)),
+            Event::Keyboard(KeyEvent {
+                code: Key::Function(10),
+                modifiers: KeyModifiers::NONE,
+            }) => Some(ApplicationMessage::Close),
+            _ => None,
+        }
     }
 }
