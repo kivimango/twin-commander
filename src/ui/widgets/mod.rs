c072e61c99db80fb8dfaa1ec58cc32131eb585a9
kivimango <balazsviktor23@gmail.com>
2024-03-17 09:08:15 +0100

    Removed Widgets enum

--- src/ui/widgets/mod.rs
+++ src/ui/widgets/mod.rs
@@ -8,14 +8,7 @@ mod text_file_viewer;
 pub use self::panel::*;
 pub use self::text_file_viewer::*;
 
-/// A list of available widgets to use in a `Panel`.
-#[allow(unused)]
-pub enum Widgets {
-    /// Displays the content of the working directory in a table-like format.
-    Table,
-    /// Displays the contents of a text file.
-    TextFileViewer,
-}
+
 
 pub trait RenderWidget {
     /// Renders the representation of the actual state into the terminal.
