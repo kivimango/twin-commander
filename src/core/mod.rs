43fe29a51d5205f1e25bd135df036314ae6805fd
kivimango <balazsviktor23@gmail.com>
2024-07-25 13:27:20 +0200

    Expose sort module

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,6 @@
 pub mod config;
 pub mod list_dir;
+pub mod sort;
 
 pub fn calculate_progress_percentage(partial_bytes: u64, total_bytes: u64) -> u64 {
     if partial_bytes != 0 && total_bytes != 0 {
